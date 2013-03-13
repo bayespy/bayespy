Fix the sign of a log-determinant term as gaussian_logpdf and gaussian_entropy have been changed

bayespy/inference/vmp/nodes/tests/test_gaussian_markov_chain.py
@@ -147,7 +147,7 @@ class TestGaussianMarkovChain(unittest.TestCase):
                                          np.einsum('...ij,...ij', 
                                                    mumu0,
                                                    icov0),
-                                         ldet,
+                                         -ldet,
                                          N*D)
                                                    
         # The VB bound from the net