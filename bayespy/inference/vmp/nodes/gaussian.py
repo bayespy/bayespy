baad68cd0050c7b144874e21b55878be4b932b75
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2013-10-10 17:55:11 +0300

    MAINT: Fixed the license to strict GPLv3 in source code files

--- bayespy/inference/vmp/nodes/gaussian.py
+++ bayespy/inference/vmp/nodes/gaussian.py
@@ -12,7 +12,7 @@
 # under the terms of the GNU General Public License version 3 as
 # published by the Free Software Foundation.
 #
-# Bayespy is distributed in the hope that it will be useful, but
+# BayesPy is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
