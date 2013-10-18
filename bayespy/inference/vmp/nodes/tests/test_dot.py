e586f0b23a236a634050fbe9d9de78f1d36da5ee
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2013-10-18 14:14:30 +0300

    TST: Add tests for the message to parents in SumMultiply

--- bayespy/inference/vmp/nodes/tests/test_dot.py
+++ bayespy/inference/vmp/nodes/tests/test_dot.py
@@ -411,93 +411,434 @@ class TestSumMultiply(TestCase):
         data = 2
         tau = 3
         
-        def check_message(true_m0, true_m1, parent, *args, **kwargs):
-            A = SumMultiply(*args, **kwargs)
-            B = GaussianArrayARD(A, tau)
-            B.observe(data*np.ones(A.plates + A.dims[0]))
+        def check_message(true_m0, true_m1, parent, *args, F=None):
+            if F is None:
+                A = SumMultiply(*args)
+                B = GaussianArrayARD(A, tau)
+                B.observe(data*np.ones(A.plates + A.dims[0]))
+            else:
+                A = F
             (A_m0, A_m1) = A._message_to_parent(parent)
             self.assertAllClose(true_m0, A_m0)
             self.assertAllClose(true_m1, A_m1)
             pass
 
-        # Do nothing for 2-D array
-        Y = GaussianArrayARD(np.random.randn(5,3,2),
-                             np.random.rand(5,3,2),
-                             plates=(5,),
-                             shape=(3,2))
-        y = Y.get_moments()
-        m0 = tau * data * np.ones(np.shape(y[0]))
-        m1 = -0.5 * tau * utils.identity(*(Y.dims[0]))
-        check_message(m0, m1, 0,
-                      'ij->ij',
-                      Y)
-        check_message(m0, m1, 0,
-                      Y,
-                      [0,1],
-                      [0,1])
-
-
-        # Sum over the rows of a matrix
-        Y = GaussianArrayARD(np.random.randn(5,3,2),
-                             np.random.rand(5,3,2),
-                             plates=(5,),
-                             shape=(3,2))
-        y = Y.get_moments()
-        m0 = tau * data * np.ones((5,3,2))
-        m1 = -0.5 * tau * utils.identity(1,2) * np.ones((3,2,3,2))
-        check_message(m0, m1, 0,
-                      'ij->j',
-                      Y)
-        check_message(m0, m1, 0,
-                      Y,
-                      [0,1],
-                      [1])
-
-        # Inner product of three vectors
+        # Check: different message to each of multiple parents
         X1 = GaussianArrayARD(np.random.randn(2),
-                              np.random.rand(2),
-                              plates=(),
-                              shape=(2,))
+                              np.random.rand(2))
         x1 = X1.get_moments()
-        X2 = GaussianArrayARD(np.random.randn(6,1,2),
-                              np.random.rand(6,1,2),
-                              plates=(6,1),
-                              shape=(2,))
+        X2 = GaussianArrayARD(np.random.randn(2),
+                              np.random.rand(2))
         x2 = X2.get_moments()
-        X3 = GaussianArrayARD(np.random.randn(7,6,5,2),
-                              np.random.rand(7,6,5,2),
-                              plates=(7,6,5),
-                              shape=(2,))
-        x3 = X3.get_moments()
-        m0 = tau * data * np.sum(x2[0] * x3[0], axis=(0,1,2))
-        m1 = -0.5 * tau * np.sum(x2[1] * x3[1], axis=(0,1,2))
+        m0 = tau * data * x2[0]
+        m1 = -0.5 * tau * x2[1] * np.identity(2)
         check_message(m0, m1, 0,
-                      'i,i,i',
+                      'i,i->i',
                       X1,
-                      X2,
-                      X3)
-        check_message(m0, m1, 0,
-                      'i,i,i->',
-                      X1,
-                      X2,
-                      X3)
+                      X2)
         check_message(m0, m1, 0,
                       X1,
                       [9],
                       X2,
                       [9],
-                      X3,
+                      [9])
+        m0 = tau * data * x1[0]
+        m1 = -0.5 * tau * x1[1] * np.identity(2)
+        check_message(m0, m1, 1,
+                      'i,i->i',
+                      X1,
+                      X2)
+        check_message(m0, m1, 1,
+                      X1,
+                      [9],
+                      X2,
+                      [9],
+                      [9])
+        
+        # Check: key not in output
+        X1 = GaussianArrayARD(np.random.randn(2),
+                              np.random.rand(2))
+        x1 = X1.get_moments()
+        m0 = tau * data * np.ones(2)
+        m1 = -0.5 * tau * np.ones((2,2))
+        check_message(m0, m1, 0,
+                      'i',
+                      X1)
+        check_message(m0, m1, 0,
+                      'i->',
+                      X1)
+        check_message(m0, m1, 0,
+                      X1,
                       [9])
         check_message(m0, m1, 0,
                       X1,
                       [9],
+                      [])
+
+        # Check: key not in some input
+        X1 = GaussianArrayARD(np.random.randn(),
+                              np.random.rand())
+        x1 = X1.get_moments()
+        X2 = GaussianArrayARD(np.random.randn(2),
+                              np.random.rand(2))
+        x2 = X2.get_moments()
+        m0 = tau * data * np.sum(x2[0], axis=-1)
+        m1 = -0.5 * tau * np.sum(x2[1] * np.identity(2),
+                                 axis=(-1,-2))
+        check_message(m0, m1, 0,
+                      ',i->i',
+                      X1,
+                      X2)
+        check_message(m0, m1, 0,
+                      X1,
+                      [],
                       X2,
                       [9],
-                      X3,
+                      [9])
+        m0 = tau * data * x1[0] * np.ones(2)
+        m1 = -0.5 * tau * x1[1] * np.identity(2)
+        check_message(m0, m1, 1,
+                      ',i->i',
+                      X1,
+                      X2)
+        check_message(m0, m1, 1,
+                      X1,
+                      [],
+                      X2,
                       [9],
-                      [])
-                            
+                      [9])
+
+        # Check: keys in different order
+        Y1 = GaussianArrayARD(np.random.randn(3,2),
+                              np.random.rand(3,2))
+        y1 = Y1.get_moments()
+        Y2 = GaussianArrayARD(np.random.randn(2,3),
+                              np.random.rand(2,3))
+        y2 = Y2.get_moments()
+        m0 = tau * data * y2[0].T
+        m1 = -0.5 * tau * np.einsum('ijlk->jikl', y2[1] * utils.identity(2,3))
+        check_message(m0, m1, 0,
+                      'ij,ji->ij',
+                      Y1,
+                      Y2)
+        check_message(m0, m1, 0,
+                      Y1,
+                      ['i','j'],
+                      Y2,
+                      ['j','i'],
+                      ['i','j'])
+        m0 = tau * data * y1[0].T
+        m1 = -0.5 * tau * np.einsum('ijlk->jikl', y1[1] * utils.identity(3,2))
+        check_message(m0, m1, 1,
+                      'ij,ji->ij',
+                      Y1,
+                      Y2)
+        check_message(m0, m1, 1,
+                      Y1,
+                      ['i','j'],
+                      Y2,
+                      ['j','i'],
+                      ['i','j'])
+
+        # Check: plates when different dimensionality
+        X1 = GaussianArrayARD(np.random.randn(5),
+                              np.random.rand(5),
+                              shape=(),
+                              plates=(5,))
+        x1 = X1.get_moments()
+        X2 = GaussianArrayARD(np.random.randn(5,3),
+                              np.random.rand(5,3),
+                              shape=(3,),
+                              plates=(5,))
+        x2 = X2.get_moments()
+        m0 = tau * data * np.sum(np.ones((5,3)) * x2[0], axis=-1)
+        m1 = -0.5 * tau * np.sum(x2[1] * utils.identity(3), axis=(-1,-2))
+        check_message(m0, m1, 0,
+                      ',i->i',
+                      X1,
+                      X2)
+        check_message(m0, m1, 0,
+                      X1,
+                      [],
+                      X2,
+                      ['i'],
+                      ['i'])
+        m0 = tau * data * x1[0][:,np.newaxis] * np.ones((5,3))
+        m1 = -0.5 * tau * x1[1][:,np.newaxis,np.newaxis] * utils.identity(3)
+        check_message(m0, m1, 1,
+                      ',i->i',
+                      X1,
+                      X2)
+        check_message(m0, m1, 1,
+                      X1,
+                      [],
+                      X2,
+                      ['i'],
+                      ['i'])
+        
+        # Check: other parent's moments broadcasts over plates when node has the
+        # same plates
+        X1 = GaussianArrayARD(np.random.randn(5,4,3),
+                              np.random.rand(5,4,3),
+                              shape=(3,),
+                              plates=(5,4))
+        x1 = X1.get_moments()
+        X2 = GaussianArrayARD(np.random.randn(3),
+                              np.random.rand(3),
+                              shape=(3,),
+                              plates=(5,4))
+        x2 = X2.get_moments()
+        m0 = tau * data * np.ones((5,4,3)) * x2[0]
+        m1 = -0.5 * tau * x2[1] * utils.identity(3)
+        check_message(m0, m1, 0,
+                      'i,i->i',
+                      X1,
+                      X2)
+        check_message(m0, m1, 0,
+                      X1,
+                      ['i'],
+                      X2,
+                      ['i'],
+                      ['i'])
+        
+        # Check: other parent's moments broadcasts over plates when node does
+        # not have that plate
+        X1 = GaussianArrayARD(np.random.randn(3),
+                              np.random.rand(3),
+                              shape=(3,),
+                              plates=())
+        x1 = X1.get_moments()
+        X2 = GaussianArrayARD(np.random.randn(3),
+                              np.random.rand(3),
+                              shape=(3,),
+                              plates=(5,4))
+        x2 = X2.get_moments()
+        m0 = tau * data * np.sum(np.ones((5,4,3)) * x2[0], axis=(0,1))
+        m1 = -0.5 * tau * np.sum(np.ones((5,4,1,1))
+                                 * utils.identity(3)
+                                 * x2[1], 
+                                 axis=(0,1))
+        check_message(m0, m1, 0,
+                      'i,i->i',
+                      X1,
+                      X2)
+        check_message(m0, m1, 0,
+                      X1,
+                      ['i'],
+                      X2,
+                      ['i'],
+                      ['i'])
+        
+        # Check: other parent's moments broadcasts over plates when the node
+        # only broadcasts that plate
+        X1 = GaussianArrayARD(np.random.randn(3),
+                              np.random.rand(3),
+                              shape=(3,),
+                              plates=(1,1))
+        x1 = X1.get_moments()
+        X2 = GaussianArrayARD(np.random.randn(3),
+                              np.random.rand(3),
+                              shape=(3,),
+                              plates=(5,4))
+        x2 = X2.get_moments()
+        m0 = tau * data * np.sum(np.ones((5,4,3)) * x2[0], axis=(0,1), keepdims=True)
+        m1 = -0.5 * tau * np.sum(np.ones((5,4,1,1))
+                                 * utils.identity(3)
+                                 * x2[1], 
+                                 axis=(0,1),
+                                 keepdims=True)
+        check_message(m0, m1, 0,
+                      'i,i->i',
+                      X1,
+                      X2)
+        check_message(m0, m1, 0,
+                      X1,
+                      ['i'],
+                      X2,
+                      ['i'],
+                      ['i'])
+        
+        # Check: broadcasted dimensions
+        X1 = GaussianArrayARD(np.random.randn(1,1),
+                              np.random.rand(1,1))
+        x1 = X1.get_moments()
+        X2 = GaussianArrayARD(np.random.randn(3,2),
+                              np.random.rand(3,2))
+        x2 = X2.get_moments()
+        m0 = tau * data * np.sum(np.ones((3,2)) * x2[0], 
+                                 keepdims=True)
+        m1 = -0.5 * tau * np.sum(utils.identity(3,2) * x2[1], 
+                                 keepdims=True)
+        check_message(m0, m1, 0,
+                      'ij,ij->ij',
+                      X1,
+                      X2)
+        check_message(m0, m1, 0,
+                      X1,
+                      [0,1],
+                      X2,
+                      [0,1],
+                      [0,1])
+        m0 = tau * data * np.ones((3,2)) * x1[0]
+        m1 = -0.5 * tau * utils.identity(3,2) * x1[1]
+        check_message(m0, m1, 1,
+                      'ij,ij->ij',
+                      X1,
+                      X2)
+        check_message(m0, m1, 1,
+                      X1,
+                      [0,1],
+                      X2,
+                      [0,1],
+                      [0,1])
+
+        # Check: non-ARD observations
+        X1 = GaussianArrayARD(np.random.randn(2),
+                              np.random.rand(2))
+        x1 = X1.get_moments()
+        Lambda = np.array([[2, 1.5], [1.5, 2]])
+        F = SumMultiply('i->i', X1)
+        Y = Gaussian(F, Lambda)
+        y = np.random.randn(2)
+        Y.observe(y)
+        m0 = np.dot(Lambda, y)
+        m1 = -0.5 * Lambda
+        check_message(m0, m1, 0,
+                      'i->i',
+                      X1,
+                      F=F)
+        check_message(m0, m1, 0,
+                      X1,
+                      ['i'],
+                      ['i'],
+                      F=F)
+
+        # Check: mask with same shape
+        X1 = GaussianArrayARD(np.random.randn(3,2),
+                              np.random.rand(3,2),
+                              shape=(2,),
+                              plates=(3,))
+        x1 = X1.get_moments()
+        mask = np.array([True, False, True])
+        F = SumMultiply('i->i', X1)
+        Y = GaussianArrayARD(F, tau)                     
+        Y.observe(data*np.ones((3,2)), mask=mask)
+        m0 = tau * data * mask[:,np.newaxis] * np.ones(2)
+        m1 = -0.5 * tau * mask[:,np.newaxis,np.newaxis] * np.identity(2)
+        check_message(m0, m1, 0,
+                      'i->i',
+                      X1,
+                      F=F)
+        check_message(m0, m1, 0,
+                      X1,
+                      ['i'],
+                      ['i'],
+                      F=F)
+
+        # Check: mask larger
+        X1 = GaussianArrayARD(np.random.randn(2),
+                              np.random.rand(2),
+                              shape=(2,),
+                              plates=())
+        x1 = X1.get_moments()
+        X2 = GaussianArrayARD(np.random.randn(3,2),
+                              np.random.rand(3,2),
+                              shape=(2,),
+                              plates=(3,))
+        x2 = X2.get_moments()
+        mask = np.array([True, False, True])
+        F = SumMultiply('i,i->i', X1, X2)
+        Y = GaussianArrayARD(F, tau,
+                             plates=(3,))                     
+        Y.observe(data*np.ones((3,2)), mask=mask)
+        m0 = tau * data * np.sum(mask[:,np.newaxis] * x2[0], axis=0)
+        m1 = -0.5 * tau * np.sum(mask[:,np.newaxis,np.newaxis]
+                                 * x2[1]
+                                 * np.identity(2),
+                                 axis=0)
+        check_message(m0, m1, 0,
+                      'i,i->i',
+                      X1,
+                      X2,
+                      F=F)
+        check_message(m0, m1, 0,
+                      X1,
+                      ['i'],
+                      X2,
+                      ['i'],
+                      ['i'],
+                      F=F)
+
+        # Check: mask for broadcasted plate
+        X1 = GaussianArrayARD(np.random.randn(2),
+                              np.random.rand(2),
+                              plates=(1,))
+        x1 = X1.get_moments()
+        X2 = GaussianArrayARD(np.random.randn(2),
+                              np.random.rand(2),
+                              plates=(3,))
+        x2 = X2.get_moments()
+        mask = np.array([True, False, True])
+        F = SumMultiply('i,i->i', X1, X2)
+        Y = GaussianArrayARD(F, tau,
+                             plates=(3,))
+        Y.observe(data*np.ones((3,2)), mask=mask)
+        m0 = tau * data * np.sum(mask[:,np.newaxis] * x2[0], 
+                                 axis=0,
+                                 keepdims=True)
+        m1 = -0.5 * tau * np.sum(mask[:,np.newaxis,np.newaxis]
+                                 * x2[1]
+                                 * np.identity(2),
+                                 axis=0,
+                                 keepdims=True)
+        check_message(m0, m1, 0,
+                      'i->i',
+                      X1,
+                      F=F)
+        check_message(m0, m1, 0,
+                      X1,
+                      ['i'],
+                      ['i'],
+                      F=F)
+
         pass
+
+def check_performance(scale=1e2):
+    """
+    Tests that the implementation of SumMultiply is efficient.
+
+    This is not a unit test (not run automatically), but rather a
+    performance test, which you may run to test the performance of the
+    node. A naive implementation of SumMultiply will run out of memory in
+    some cases and this method checks that the implementation is not naive
+    but good.
+    """
+
+    # Check: Broadcasted plates are computed efficiently
+    # (bad implementation will take a long time to run)
+    s = scale
+    X1 = GaussianArrayARD(np.random.randn(s,s),
+                          np.random.rand(s,s),
+                          shape=(s,),
+                          plates=(s,))
+    X2 = GaussianArrayARD(np.random.randn(s,1,s),
+                          np.random.rand(s,1,s),
+                          shape=(s,),
+                          plates=(s,1))
+    F = SumMultiply('i,i', X1, X2)
+    Y = GaussianArrayARD(F, 1)
+    Y.observe(np.ones((s,s)))
+    try:
+        F._message_to_parent(1)
+    except e:
+        print(e)
+        print('SOMETHING BAD HAPPENED')
+
+
+    # Check: Broadcasted dimensions are computed efficiently
+    # (bad implementation will run out of memory)
+
+    pass
 
 
         
