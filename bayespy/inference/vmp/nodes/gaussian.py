BUG: Fix CGF in the rotation method of Gaussian ARD arrays
Rotate method was incorrect for Gaussian arrays with more than one
variable axes. The update of CGF in the rotation of Gaussian ARD arrays
did not take into account other variable axes. Now, CGF is multiplied by
the product of variable axes that were not rotated.

bayespy/inference/vmp/nodes/gaussian.py
@@ -1115,7 +1115,7 @@ def _GaussianArrayARD(shape, shape_mu=None):
                     # Because the covariance matrix has shape (1,1,...,1,1),
                     # that is 2*D number of ones, remove the extra half of the
                     # shape
-                    phi1 = np.reshape(phi1, np.shape(phi1)[:-2*D] + D*(1,))                  
+                    phi1 = np.reshape(phi1, np.shape(phi1)[:-2*D] + D*(1,))
                     
                 std = np.sqrt(-0.5 / phi1)
                 mu = self.u[0]
@@ -1175,7 +1175,9 @@ def _GaussianArrayARD(shape, shape_mu=None):
             self.u[1] = rotate_covariance(self.u[1], R, 
                                           axis=axis,
                                           ndim=ndim)
-            self.g -= logdetR
+            s = list(self.dims[0])
+            s.pop(axis)
+            self.g -= logdetR * np.prod(s)
 
             return
 