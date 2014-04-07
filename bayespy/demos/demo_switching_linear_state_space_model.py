84bd658f29d210d649dbe505c70fc7ede833ea15
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2014-04-07 12:44:40 +0300

    ENH: Improve switching LSSM demo

--- bayespy/demos/demo_switching_linear_state_space_model.py
+++ bayespy/demos/demo_switching_linear_state_space_model.py
@@ -86,12 +86,14 @@ def switching_linear_state_space_model(K=3, D=10, N=100, M=20):
                     plates=(K,M,1),
                     name='C',
                     plotter=bpplt.GaussianHintonPlotter())
+    C.initialize_from_value(np.random.randn(K,M,1,D))
 
     # Underlying noiseless function
     # (K,M,N) x ()
     F = SumMultiply('i,i', 
                     C, 
-                    X)
+                    X,
+                    name='F')
     
     #
     # Switching dynamics (HMM)
@@ -105,7 +107,7 @@ def switching_linear_state_space_model(K=3, D=10, N=100, M=20):
     V = Dirichlet(1e-3*np.ones(K),
                   plates=(K,),
                   name='V')
-    V.initialize_from_value(1000*np.identity(K) + 1*np.ones((K,K)))
+    V.initialize_from_value(10*np.identity(K) + 1*np.ones((K,K)))
 
     # Hidden states (with unknown initial state probabilities and state
     # transition probabilities)
@@ -122,31 +124,39 @@ def switching_linear_state_space_model(K=3, D=10, N=100, M=20):
     tau = Gamma(1e-5,
                 1e-5,
                 name='tau')
-    tau.initialize_from_value(1)
+    tau.initialize_from_value(1e4)
 
     # Emission/observation distribution
     Y = Mixture(Z, GaussianARD, F, tau,
                 cluster_plate=-3,
                 name='Y')
 
-    Q = VB(Y, 
+    Q = VB(Y, F,
            Z, rho, V,
            C, gamma, X, A, alpha,
            tau)
 
     return Q
 
-def run_slssm(y, D, K, seed=42, rotate=True, debug=False, maxiter=100, monitor=False):
+def run_slssm(y, D, K, rotate=True, debug=False, maxiter=100, mask=True,
+              monitor=False, update_hyper=0):
+    
     (M, N) = np.shape(y)
 
     # Construct model
-    Q = switching_linear_state_space_model(M=1, K=K, N=N, D=D)
+    Q = switching_linear_state_space_model(M=M, K=K, N=N, D=D)
 
-    # Add observations/data
-    Q['Y'].observe(y)
+    Q['Y'].observe(y, mask=mask)
 
     # Set up rotation speed-up
     if rotate:
+        # Initial rotate the D-dimensional state space (X, A, C)
+        # Do not update hyperparameters
+        rotA_init = transformations.RotateGaussianARD(Q['A'])
+        rotX_init = transformations.RotateGaussianMarkovChain(Q['X'], 
+                                                              rotA_init)
+        rotC_init = transformations.RotateGaussianARD(Q['C'])
+        R_init = transformations.RotationOptimizer(rotX_init, rotC_init, D)
         # Rotate the D-dimensional state space (X, A, C)
         rotA = transformations.RotateGaussianARD(Q['A'], 
                                                  Q['alpha'])
@@ -163,13 +173,21 @@ def run_slssm(y, D, K, seed=42, rotate=True, debug=False, maxiter=100, monitor=F
             rotate_kwargs = {'maxiter': 10}
 
     # Run inference
+    if monitor:
+        Q.plot()
     for n in range(maxiter):
-        #Q.update('C', 'Z', 'X', 'A', 'tau', plot=monitor)
-        Q.update(plot=monitor)
-        R.rotate(**rotate_kwargs)
+        if n < update_hyper:
+            Q.update('Z', 'X', 'C', 'A', 'tau', plot=monitor)
+            R_init.rotate(**rotate_kwargs)
+        else:
+            Q.update(plot=monitor)
+            R.rotate(**rotate_kwargs)
+
+    return Q
     
 
-def run(N=1000, maxiter=30, D=2, K=3, seed=42, plot=True, debug=False, monitor=True):
+def run(N=1000, maxiter=100, D=2, K=3, seed=42, plot=True, debug=False, 
+        monitor=True):
 
     # Use deterministic random numbers
     if seed is not None:
@@ -179,49 +197,102 @@ def run(N=1000, maxiter=30, D=2, K=3, seed=42, plot=True, debug=False, monitor=T
     # Generate data
     #
 
-    # Two states: 1) oscillation, 2) random walk
-    w1 = 0.02 * 2*np.pi
-    A = [ [[np.cos(w1), -np.sin(w1)],
-           [np.sin(w1), np.cos(w1)]],
-          [[1.0, 0.0],
-           [0.0, 1.0]] ]
-    C = [ [[1.0, 0.0]],
-          [[0.5, 0.5]] ]
+    case = 1
+    if case == 1:
 
-    # State switching probabilities
-    q = 0.98         # probability to stay in the same state
-    r = (1-q)/(2-1) # probability to switch
-    P = q*np.identity(2) + r*(np.ones((2,2))-np.identity(2))
-    
-    X = np.zeros((N, 2))
-    Z = np.zeros(N)
-    Y = np.zeros(N)
-    z = np.random.randint(2)
-    x = np.random.randn(2)
-    Z[0] = z
-    X[0,:] = x
-    for n in range(1,N):
-        x = np.dot(A[z], x) + np.random.randn(2)
-        y = np.dot(C[z], x) + np.random.randn()
-        z = np.random.choice(2, p=P[z])
+        # Oscillation
+        y0 = 5 * np.sin(0.02*2*np.pi * np.arange(N))
+        # Random walk
+        y1 = np.cumsum(np.random.randn(N))
+        y = [y0, y1]
 
-        Z[n] = z
-        X[n,:] = x
-        Y[n] = y
+        # State switching probabilities
+        q = 0.99        # probability to stay in the same state
+        r = (1-q)/(2-1)  # probability to switch
+        P = q*np.identity(2) + r*(np.ones((2,2))-np.identity(2))
+
+        Y = np.zeros(N)
+        z = np.random.randint(2)
+        for n in range(1,N):
+            Y[n] = y[z][n]
+            z = np.random.choice(2, p=P[z])
+        Y = Y[None,:]
+            
+    elif case == 2:
+
+        # Two states: 1) oscillation, 2) random walk
+        w1 = 0.02 * 2*np.pi
+        A = [[np.cos(w1), -np.sin(w1),   0],
+             [np.sin(w1),  np.cos(w1),   0],
+             [         0,           0, 1.0]]
+        C = [ [[1.0, 0.0, 0.0]],
+              [[0.0, 0.0, 1.0]] ]
+
+        # State switching probabilities
+        q = 0.993        # probability to stay in the same state
+        r = (1-q)/(2-1)  # probability to switch
+        P = q*np.identity(2) + r*(np.ones((2,2))-np.identity(2))
+
+        X = np.zeros((N, 3))
+        Z = np.zeros(N)
+        Y = np.zeros(N)
+        z = np.random.randint(2)
+        x = np.random.randn(3)
+        Z[0] = z
+        X[0,:] = x
+        for n in range(1,N):
+            x = np.dot(A, x) + np.random.randn(3)
+            y = np.dot(C[z], x) + np.random.randn()
+            z = np.random.choice(2, p=P[z])
+
+            Z[n] = z
+            X[n,:] = x
+            Y[n] = y
+        Y = Y[None,:]
+
+    elif case == 3:
+
+        # This experiment is from "Variational Learning for Switching
+        # State-Space Models" (Ghahramani and Hinton)
+
+        x0 = np.zeros(N)
+        x0[0] = np.random.randn()
+        for n in range(1,N):
+            x0[n] = 0.99*x0[n-1] + np.random.randn()
+
+        x1 = np.zeros(N)
+        x1[0] = 10 * np.random.randn()
+        for n in range(1,N):
+            x1[n] = 0.9*x1[n-1] + np.sqrt(10)*np.random.randn()
+
+        # State switching probabilities
+        q = 0.95        # probability to stay in the same state
+        r = (1-q)/(2-1)  # probability to switch
+        P = 0.9*np.identity(2) + 0.05*np.ones((2,2))
+
+        Y = np.zeros(N)
+        z = np.random.randint(2)
+        x = np.array([x0,x1])
+        for n in range(N):
+            y = x[z,n]
+            z = np.random.choice(2, p=P[z])
+            Y[n] = y
+        Y = Y[None,:]
+        
 
     if plot:
         plt.figure()
-        plt.plot(Y)
+        bpplt.timeseries(Y, axis=-1)
 
     #
     # Use switching linear state-space model for inference
     #
 
-    Q = run_slssm(Y[None,:], D, K, 
-                  seed=seed, 
+    Q = run_slssm(Y, D, K, 
                   debug=debug,
                   maxiter=maxiter,
-                  monitor=monitor)
+                  monitor=monitor,
+                  update_hyper=5)
 
 
     #
