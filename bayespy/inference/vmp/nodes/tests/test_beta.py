58ab8c317c71aa2c5fc8d47d2bb9e4778290482e
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2014-04-29 15:55:50 +0300

    TST: Add unit tests for Beta node

---
+++ bayespy/inference/vmp/nodes/tests/test_beta.py
@@ -0,0 +1,106 @@
+######################################################################
+# Copyright (C) 2014 Jaakko Luttinen
+#
+# This file is licensed under Version 3.0 of the GNU General Public
+# License. See LICENSE for a text of the license.
+######################################################################
+
+######################################################################
+# This file is part of BayesPy.
+#
+# BayesPy is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# BayesPy is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BayesPy.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################
+
+"""
+Unit tests for `beta` module.
+"""
+
+import numpy as np
+from scipy import special
+
+from bayespy.nodes import Beta
+
+from bayespy.utils import utils
+from bayespy.utils import random
+
+from bayespy.utils.utils import TestCase
+
+class TestBinomial(TestCase):
+    """
+    Unit tests for Binomial node
+    """
+
+    
+    def test_init(self):
+        """
+        Test the creation of binomial nodes.
+        """
+
+        # Some simple initializations
+        p = Beta([1.5, 4.2])
+
+        # Check that plates are correct
+        p = Beta([2, 3], plates=(4,3))
+        self.assertEqual(p.plates,
+                         (4,3))
+        p = Beta(np.ones((4,3,2)))
+        self.assertEqual(p.plates,
+                         (4,3))
+
+        # Parent not a vector
+        self.assertRaises(ValueError,
+                          Beta,
+                          4)
+        
+        # Parent vector has wrong shape
+        self.assertRaises(ValueError,
+                          Beta,
+                          [4])
+        self.assertRaises(ValueError,
+                          Beta,
+                          [4,4,4])
+
+        # Parent vector has invalid values
+        self.assertRaises(ValueError,
+                          Beta,
+                          [-2,3])
+
+        # Plates inconsistent
+        self.assertRaises(ValueError,
+                          Beta,
+                          np.ones((4,2)),
+                          plates=(3,))
+
+        # Explicit plates too small
+        self.assertRaises(ValueError,
+                          Beta,
+                          np.ones((4,2)),
+                          plates=(1,))
+
+        pass
+
+    
+    def test_moments(self):
+        """
+        Test the moments of binomial nodes.
+        """
+
+        p = Beta([2, 3])
+        u = p._message_to_child()
+        self.assertAllClose(u[0],
+                            special.psi([2,3]) - special.psi(2+3))
+        
+        pass
+
+    
+
