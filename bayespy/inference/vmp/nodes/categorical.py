193490f22f99174c636bc4c6ad3accbd3ce685ed
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2014-04-30 15:18:55 +0300

    MAINT: Categorical is now a special case of Multinomial
    Categorical node, distribution and moments are subclasses of the
    corresponding multinomial classes.

--- bayespy/inference/vmp/nodes/categorical.py
+++ bayespy/inference/vmp/nodes/categorical.py
@@ -21,28 +21,47 @@
 # along with BayesPy.  If not, see <http://www.gnu.org/licenses/>.
 ######################################################################
 
+"""
+Module for the categorical distribution node.
+"""
+
 import numpy as np
 
-from .expfamily import ExponentialFamily
-from .expfamily import ExponentialFamilyDistribution
+#from .expfamily import ExponentialFamily
+#from .expfamily import ExponentialFamilyDistribution
 from .expfamily import useconstructor
-from .dirichlet import Dirichlet, DirichletMoments
-from .node import Moments, ensureparents
+from .multinomial import (MultinomialMoments,
+                          MultinomialDistribution,
+                          Multinomial)
+#from .dirichlet import Dirichlet, DirichletMoments
+from .node import ensureparents
 
 from bayespy.utils import random
 from bayespy.utils import utils
 
 
-class CategoricalMoments(Moments):
+class CategoricalMoments(MultinomialMoments):
+    """
+    Class for the moments of categorical variables.
+    """
+
     ndim_observations = 0
+
     
     def __init__(self, categories):
+        """
+        Create moments object for categorical variables
+        """
         self.D = categories
         super().__init__()
+
         
     def compute_fixed_moments(self, x):
-        """ Compute u(x) and f(x) for given x. """
+        """
+        Compute the moments for a fixed value
+        """
 
+        # Check that x is valid
         x = np.asanyarray(x)
         if not utils.isinteger(x):
             raise ValueError("Values must be integers")
@@ -54,77 +73,107 @@ class CategoricalMoments(Moments):
         u0 = np.reshape(u0, np.shape(x) + (self.D,))
 
         return [u0]
+    
 
     def compute_dims_from_values(self, x):
+        """
+        Return the shape of the moments for a fixed value.
+
+        The observations are scalar.
+        """
         return ( (self.D,), )
 
 
-class CategoricalDistribution(ExponentialFamilyDistribution):
+class CategoricalDistribution(MultinomialDistribution):
+    """
+    Class for the VMP formulas of categorical variables.
+    """    
 
-    ndims = (1,)
-    ndims_parents = ( (1,), )
+    def __init__(self, categories):
+        """
+        Create VMP formula node for a categorical variable
 
-    def __init__(self, D):
-        self.D = D
+        `categories` is the total number of categories.
+        """
+        if not isinstance(categories, int):
+            raise ValueError("Number of categories must be integer")
+        if categories < 0:
+            raise ValueError("Number of categoriess must be non-negative")
+        self.D = categories
+        super().__init__(1)
+        
 
-    def compute_message_to_parent(self, parent, index, u, *u_parents):
-        """ . """
-        if index == 0:
-            return [ u[0].copy() ]
-        else:
-            raise ValueError("Index out of bounds")
+    def compute_message_to_parent(self, parent, index, u, u_p):
+        """
+        Compute the message to a parent node.
+        """
+        return super().compute_message_to_parent(parent, index, u, u_p)
 
 
-    def compute_phi_from_parents(self, *u_parents, mask=True):
-        return [u_parents[0][0]]
+    def compute_phi_from_parents(self, u_p, mask=True):
+        """
+        Compute the natural parameter vector given parent moments.
+        """
+        return super().compute_phi_from_parents(u_p, mask=mask)
 
 
     def compute_moments_and_cgf(self, phi, mask=True):
-        # For numerical reasons, scale contributions closer to
-        # one, i.e., subtract the maximum of the log-contributions.
-        max_phi = np.max(phi[0], axis=-1, keepdims=True)
-        p = np.exp(phi[0]-max_phi)
-        sum_p = np.sum(p, axis=-1, keepdims=True)
-        # Moments
-        u0 = p / sum_p
-        u = [u0]
-        # G
-        g = -np.log(sum_p) - max_phi
-        g = np.squeeze(g, axis=-1)
-        #print('Categorical.compute_u_and_g, g:', np.sum(g), np.shape(g), np.sum(max_phi))
-        return (u, g)
+        """
+        Compute the moments and :math:`g(\phi)`.
+        """
+        return super().compute_moments_and_cgf(phi, mask=mask)
 
-    def compute_cgf_from_parents(self, *u_parents):
-        return 0
+        
+    def compute_cgf_from_parents(self, u_p):
+        """
+        Compute :math:`\mathrm{E}_{q(p)}[g(p)]`
+        """
+        return super().compute_cgf_from_parents(u_p)
+    
 
     def compute_fixed_moments_and_f(self, x, mask=True):
-        """ Compute u(x) and f(x) for given x. """
+        """
+        Compute the moments and :math:`f(x)` for a fixed value.
+        """
 
-        # TODO: You could check that x has proper dimensions
+        # Check the validity of x
         x = np.asanyarray(x)
         if not utils.isinteger(x):
             raise ValueError("Values must be integers")
         if np.any(x < 0) or np.any(x >= self.D):
             raise ValueError("Invalid category index")
 
+        # Form a binary matrix with only one non-zero (1) in the last axis
         u0 = np.zeros((np.size(x), self.D))
         u0[[np.arange(np.size(x)), np.ravel(x)]] = 1
         u0 = np.reshape(u0, np.shape(x) + (self.D,))
+        u = [u0]
+
+        # f(x) is zero
         f = 0
 
-        return ([u0], f)
+        return (u, f)
 
+    
     def shape_of_value(self, dims):
+        """
+        Return the shape of realizations
+        """
         return ()
 
 
-class Categorical(ExponentialFamily):
-    
-    _parent_moments = (DirichletMoments(),)
+class Categorical(Multinomial):
+    """
+    Node for categorical random variables.
+    """
+
 
     @useconstructor
     def __init__(self, p, **kwargs):
-        super().__init__(p, **kwargs)
+        """
+        Create categorical random variable node
+        """
+        super().__init__(1, p, **kwargs)
 
 
     @classmethod
@@ -152,16 +201,23 @@ class Categorical(ExponentialFamily):
                 distribution, 
                 moments, 
                 cls._parent_moments)
+    
 
     def random(self):
+        """
+        Draw a random sample from the distribution.
+        """
         logp = self.phi[0]
         logp -= np.amax(logp, axis=-1, keepdims=True)
         p = np.exp(logp)
         return random.categorical(p, size=self.plates)
 
+    
     def show(self):
-        p = self.u[0] #np.exp(self.phi[0])
-        #p /= np.sum(p, axis=-1, keepdims=True)
+        """
+        Print the distribution using standard parameterization.
+        """
+        p = self.u[0]
         print("%s ~ Categorical(p)" % self.name)
         print("  p = ")
         print(p)
