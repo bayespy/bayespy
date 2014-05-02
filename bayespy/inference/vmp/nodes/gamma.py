a7507a3437e715067eb09520854c796a196feea6
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2014-05-02 09:20:51 +0300

    DOC: Add docstrings to gamma module

--- bayespy/inference/vmp/nodes/gamma.py
+++ bayespy/inference/vmp/nodes/gamma.py
@@ -21,6 +21,10 @@
 # along with BayesPy.  If not, see <http://www.gnu.org/licenses/>.
 ######################################################################
 
+"""
+Module for the gamma distribution node.
+"""
+
 import numpy as np
 import scipy.special as special
 
@@ -28,8 +32,10 @@ from .node import Node
 from .deterministic import Deterministic
 from .expfamily import ExponentialFamily, ExponentialFamilyDistribution
 from .constant import Constant
+from .node import Moments
 
 from bayespy.utils import utils
+
 
 def diagonal(alpha):
     """
@@ -39,39 +45,70 @@ def diagonal(alpha):
                                    name=alpha.name + " as Wishart")
 
 
-from .node import Moments
-
 class GammaPriorMoments(Moments):
+    """
+    Class for the moments of the shape parameter in gamma distributions.
+    """
+    
     ndim_observations = 0
+
+    
     def compute_fixed_moments(self, a):
-        """ Compute moments for fixed x. """
+        """
+        Compute the moments for a fixed value
+        """
         u0 = a
         u1 = special.gammaln(a)
         return [u0, u1]
+    
 
     def compute_dims_from_values(self, a):
-        """ Compute the dimensions of phi or u. """
+        """
+        Return the shape of the moments for a fixed value.
+        """
         return [(), ()]
+    
 
 class GammaMoments(Moments):
+    """
+    Class for the moments of gamma variables.
+    """
+    
     ndim_observations = 0
+
+    
     def compute_fixed_moments(self, x):
-        """ Compute moments for fixed x. """
+        """
+        Compute the moments for a fixed value
+        """
+        if np.any(x < 0):
+            raise ValueError("Values must be positive")
         u0 = x
         u1 = np.log(x)
         return [u0, u1]
 
+
     def compute_dims_from_values(self, x):
-        """ Compute the dimensions of phi and u. """
+        """
+        Return the shape of the moments for a fixed value.
+        """
         return ( (), () )
+    
 
 class GammaDistribution(ExponentialFamilyDistribution):
+    """
+    Class for the VMP formulas of gamma variables.
+    """    
     
     ndims = (0, 0)
     ndims_parents = ( (0,0),
                       (0,0) )
+        
 
     def compute_message_to_parent(self, parent, index, u_self, *u_parents):
+        """
+        Compute the message to a parent node.
+        """
         if index == 0:
             raise Exception("No analytic solution exists")
         elif index == 1:
@@ -80,11 +117,19 @@ class GammaDistribution(ExponentialFamilyDistribution):
         else:
             raise ValueError("Index out of bounds")
 
+
     def compute_phi_from_parents(self, *u_parents, mask=True):
+        """
+        Compute the natural parameter vector given parent moments.
+        """
         return [-u_parents[1][0],
                 1*u_parents[0][0]]
+    
 
     def compute_moments_and_cgf(self, phi, mask=True):
+        """
+        Compute the moments and :math:`g(\phi)`.
+        """
         log_b = np.log(-phi[0])
         u0 = phi[1] / (-phi[0])
         u1 = special.digamma(phi[1]) - log_b
@@ -92,33 +137,55 @@ class GammaDistribution(ExponentialFamilyDistribution):
         g = phi[1] * log_b - special.gammaln(phi[1])
         return (u, g)
 
+
     def compute_cgf_from_parents(self, *u_parents):
+        """
+        Compute :math:`\mathrm{E}_{q(p)}[g(p)]`
+        """
         a = u_parents[0][0]
         gammaln_a = u_parents[0][1] #special.gammaln(a)
         b = u_parents[1][0]
         log_b = u_parents[1][1]
         g = a * log_b - gammaln_a
         return g
-        
+
+            
     def compute_fixed_moments_and_f(self, x, mask=True):
+        """
+        Compute the moments and :math:`f(x)` for a fixed value.
+        """
+        if np.any(x < 0):
+            raise ValueError("Values must be positive")
         u = [x, np.log(x)]
         f = 0
         return (u, f)
 
+
     def shape_of_value(self, dims):
-        # Dimensionality of a realization
+        """
+        Return the shape of realizations
+        """
         return ()
 
+
 class Gamma(ExponentialFamily):
+    """
+    Node for gamma random variables.
+    """
 
     dims = ( (), () )
     _distribution = GammaDistribution()
     _moments = GammaMoments()
     _parent_moments = (GammaPriorMoments(),
                        GammaMoments())
-    
+
+            
     def __init__(self, a, b, **kwargs):
+        """
+        Create gamma random variable node
+        """
         super().__init__(a, b, **kwargs)
+
 
     def random(self):
         """
@@ -127,8 +194,12 @@ class Gamma(ExponentialFamily):
         return np.random.gamma(self.phi[1],
                                -1/self.phi[0],
                                size=self.plates)
+
     
     def show(self):
+        """
+        Print the distribution using standard parameterization.
+        """
         a = self.phi[1]
         b = -self.phi[0]
         print("%s ~ Gamma(a, b)" % self.name)
@@ -138,6 +209,7 @@ class Gamma(ExponentialFamily):
     def as_diagonal_wishart(self):
         return _GammaToDiagonalWishart(self,
                                        name=self.name + " as Wishart")
+
 
 class _GammaToDiagonalWishart(Deterministic):
     """
