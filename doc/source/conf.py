c098685e52eaec8e2abb46e846fc000f52ae7882
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2014-05-06 12:38:42 +0300

    DOC: Change theme and fix LaTeX build of docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -122,7 +122,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'nature'
+#html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -211,6 +212,10 @@ latex_elements = {
 'preamble': r'''
 \usepackage{tikz}
 \usepackage{amssymb}
+\usetikzlibrary{shapes}
+\usetikzlibrary{fit}
+\usetikzlibrary{chains}
+\usetikzlibrary{arrows}
 '''
 #  \usetikzlibrary{shapes, fit, chains, arrows}
 #  \usetikzlibrary{bayesnet}
