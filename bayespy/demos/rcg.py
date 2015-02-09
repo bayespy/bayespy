31c9fc1cf55ae493fec120652a4589e64f4a693d
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2015-02-09 20:00:45 +0200

    DEMO: Add MoG demo to Riemannian CG demo

--- bayespy/demos/rcg.py
+++ bayespy/demos/rcg.py
@@ -27,11 +27,8 @@ Demonstrate Riemannian conjugate gradient
 
 import numpy as np
 
-from bayespy.nodes import (Beta,
-                           Bernoulli,
-                           GaussianARD,
+from bayespy.nodes import (GaussianARD,
                            Gamma,
-                           Gaussian,
                            SumMultiply)
 
 from bayespy.utils import random
@@ -40,71 +37,106 @@ from bayespy.inference.vmp.vmp import VB
 
 import bayespy.plot as bpplt
 
+from bayespy.demos import mog
+
 
 def pca():
 
     np.random.seed(41)
 
-    M = 20
-    N = 100
+    M = 10
+    N = 3000
     D = 5
 
-    # Construct the model
-    X = GaussianARD(0, 1, plates=(1,N), shape=(D,))
-    #X = Gaussian(np.zeros(D), np.identity(D), plates=(1,N))
-    alpha = Gamma(1e-3, 1e-3, plates=(D,))
-    W = GaussianARD(0, alpha, plates=(M,1), shape=(D,))
-    #W = Gaussian(np.zeros(D), np.identity(D), plates=(M,1))
-    #X = GaussianARD(0, 1, plates=(1,N), shape=(D,))
-    #W = GaussianARD(0, 1, plates=(M,1), shape=(D,))
+    # Construct the PCA model
+    alpha = Gamma(1e-3, 1e-3, plates=(D,), name='alpha')
+    W = GaussianARD(0, alpha, plates=(M,1), shape=(D,), name='W')
+    X = GaussianARD(0, 1, plates=(1,N), shape=(D,), name='X')
+    tau = Gamma(1e-3, 1e-3, name='tau')
     W.initialize_from_random()
     F = SumMultiply('d,d->', W, X)
-    #F = SumMultiply(',->', W, X)
-    Y = GaussianARD(F, 1e1)
+    Y = GaussianARD(F, tau, name='Y')
 
     # Observe data
-    data = np.sum(W.random() * X.random(), axis=-1)
+    data = np.sum(np.random.randn(M,1,D-1) * np.random.randn(1,N,D-1), axis=-1) + 1e-1 * np.random.randn(M,N)
     Y.observe(data)
 
     # Initialize VB engine
-    Q = VB(Y, X, W, alpha)
+    Q = VB(Y, X, W, alpha, tau)
 
     # Take one update step (so phi is ok)
-    Q.update()
-
-    # Store the state
-    p = Q.get_parameters(X, W)
+    Q.update(repeat=1)
+    Q.save()
 
     # Run VB-EM
-    Q.update(repeat=30)
+    Q.update(repeat=200)
+    bpplt.pyplot.plot(np.cumsum(Q.cputime), Q.L, 'k-')
 
     # Restore the state
-    Q.set_parameters(p, X, W)
+    Q.load()
 
     # Run Riemannian conjugate gradient
-    Q.optimize(W, maxiter=30, collapsed=[X, alpha])
+    #Q.optimize(X, alpha, maxiter=100, collapsed=[W, tau])
+    Q.optimize(W, tau, maxiter=100, collapsed=[X, alpha])
+    bpplt.pyplot.plot(np.cumsum(Q.cputime), Q.L, 'r:')
+
+    bpplt.pyplot.show()
 
 
+def mixture_of_gaussians():
+    """Collapsed Riemannian conjugate gradient demo
 
-def beta():
+    This is similar although not exactly identical to an experiment in
+    (Hensman et al 2012).
+    """
 
-    # Simple model
-    p = Beta([1, 1], plates=(3,))
-    Z = Bernoulli(p, plates=(100,3))
+    # Number of samples
+    N = 1000
 
-    # Observe data
-    data = Z.random()
-    Z.observe(data)
+    # Overlap parameter of clusters
+    R = 3
 
-    # Learn p using Riemannian gradient
-    for i in range(50):
-        d = p.get_gradient()
-        p.update_parameters(d, scale=0.5)
-        print(p.u)
+    # Construct the model
+    Q = mog.gaussianmix_model(N, 5, 2, covariance='diagonal')
 
-    p.update()
-    print(p.u)
+    # Generate data from five Gaussian clusters
+    mu = np.array([[0, 0],
+                   [R, R],
+                   [-R, R],
+                   [R, -R],
+                   [-R, -R]])
+    Z = Q['z'].random()
+    data = np.empty((N, 2))
+    for n in range(N):
+        data[n,:] = mu[Z[n]] + np.random.randn(2)
+    Q['Y'].observe(data)
 
+    # Take one update step (so phi is ok)
+    Q.update(repeat=1)
+    Q.save()
+
+    # Run standard VB-EM
+    Q.update(repeat=1000, tol=0)
+    bpplt.pyplot.plot(np.cumsum(Q.cputime), Q.L, 'k-')
+
+    # Restore the initial state
+    Q.load()
+
+    # Run Riemannian conjugate gradient
+    Q.optimize('alpha', 'X', 'Lambda', collapsed=['z'], maxiter=300, tol=0)
+    bpplt.pyplot.plot(np.cumsum(Q.cputime), Q.L, 'r:')
+
+    bpplt.pyplot.xlabel('CPU time (in seconds)')
+    bpplt.pyplot.ylabel('VB lower bound')
+    bpplt.pyplot.legend(['VB-EM', 'RCG'])
+
+    bpplt.pyplot.figure()
+    bpplt.pyplot.plot(data[:,0], data[:,1], 'rx')
+    bpplt.pyplot.title('Data')
+
+    bpplt.pyplot.show()
+    
 
 if __name__ == "__main__":
-    pca()
+    #pca()
+    mixture_of_gaussians()
