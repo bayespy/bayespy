ea7835624293cbed0a882ab40b9c8aeb54c0959f
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2015-03-13 11:34:24 +0200

    DOC: Change documentation theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -130,13 +130,16 @@ tikz_latex_preamble = """
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'sphinxdoc'
+#html_theme = 'nature'
 #html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "sidebarwidth": 300
+    }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
