2e63797833612b5d0da4cca51547fd5d2b1466d9
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2015-03-16 12:46:08 +0200

    DOC: Use README as the long description for the package

--- setup.py
+++ setup.py
@@ -23,14 +23,6 @@
 # along with BayesPy.  If not, see <http://www.gnu.org/licenses/>.
 ######################################################################
 
-LONG_DESCRIPTION    = \
-"""
-Bayesian inference tools for Python.
-
-The package provides tools for building models and performing
-posterior inference using variational Bayesian message passing.
-"""
-
 NAME         = 'bayespy'
 DESCRIPTION  = 'Bayesian inference tools for Python'
 AUTHOR       = 'Jaakko Luttinen'
@@ -40,6 +32,15 @@ LICENSE      = 'GPLv3'
 VERSION      = '0.3.1'
 
 if __name__ == "__main__":
+
+    import os
+
+    # Utility function to read the README file.
+    # Used for the long_description.  It's nice, because now 1) we have a top level
+    # README file and 2) it's easier to type in the README file than to put a raw
+    # string in below ...
+    def read(fname):
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
     from setuptools import setup, find_packages
     
@@ -59,7 +60,7 @@ if __name__ == "__main__":
           description      = DESCRIPTION,
           license          = LICENSE,
           url              = URL,
-          long_description = LONG_DESCRIPTION,
+          long_description = read('README.rst'),
           classifiers =
             [ 
               'Programming Language :: Python :: 3',
