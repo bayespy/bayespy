a4844068e839f0b035e5e0583c4f942c654e49eb
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2015-10-16 19:33:47 +0300

    REL: Bump version number to 0.3.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ AUTHOR       = 'Jaakko Luttinen'
 AUTHOR_EMAIL = 'jaakko.luttinen@iki.fi'
 URL          = 'http://bayespy.org'
 LICENSE      = 'MIT'
-VERSION      = '0.3.8'
+VERSION      = '0.3.9'
 
 if __name__ == "__main__":
 
