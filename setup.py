c575f7918f36158ffd03e1ff65b0b927353570a0
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2016-03-31 19:29:24 +0300

    DOC: Add doc dependency on nbsphinx

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ if __name__ == "__main__":
                 'sphinxcontrib-bayesnet',
                 'sphinxcontrib-bibtex',
                 'numpydoc>=0.5',
+                'nbsphinx',
             ],
         },
         packages         = find_packages(),
