BUG: Fix 0*-inf issue in lower bound computation

bayespy/inference/vmp/nodes/expfamily.py
@@ -438,8 +438,11 @@ class ExponentialFamily(Stochastic):
             # Compute the term
             phi_q = np.where(latent_mask_i, phi_q, 0)
             # Apply annealing
+            phi_diff = phi_p - T * phi_q
+            # Handle 0 * -inf
+            phi_diff = np.where(u_q != 0, phi_diff, 0)
             # TODO/FIXME: Use einsum here?
-            Z = np.sum((phi_p-T*phi_q) * u_q, axis=axis_sum)
+            Z = np.sum(phi_diff * u_q, axis=axis_sum)
 
             L = L + Z
 