Fix broadcasting in CategoricalGraph

bayespy/inference/vmp/nodes/categorical_graph.py
@@ -78,6 +78,10 @@ def map_to_plates(x, src, dst):
     )
 
 
+def map_to_shape(sizes, keys):
+    return tuple(sizes[key] for key in keys)
+
+
 class CategoricalGraph():
     """
 
@@ -190,6 +194,10 @@ class CategoricalGraph():
         self._original_sizes = {
             key: size for (key, size) in all_sizes
         }
+        self._factor_shapes = [
+            map_to_shape(self._original_sizes, variable.plates + variable.given + (variable.name,))
+            for variable in self._factor_variables
+        ]
 
         # State
         self._junctiontree = None
@@ -264,6 +272,7 @@ class CategoricalGraph():
                     xs[factor] = e * xs[factor]
             return xs
 
+        # TODO: Validate observation array shapes
 
         # Modify sizes
         self._sizes = self._original_sizes.copy()
@@ -299,7 +308,13 @@ class CategoricalGraph():
         # FIXME: Convert <log p> to exp( <log p> ). Perhaps junctiontree
         # package could support logarithms of the probabilities? Also, note
         # that these don't sum to one, they are non-normalized probabilities.
-        cpts = [np.exp(cpt.get_moments()[0]) for cpt in self._cpts]
+        cpts = [
+            np.broadcast_to(
+                np.exp(cpt.get_moments()[0]),
+                shape
+            )
+            for (shape, cpt) in zip(self._factor_shapes, self._cpts)
+        ]
 
         xs = self._slice_potentials(cpts)
         # Convert to lists..

bayespy/inference/vmp/nodes/tests/test_categorical_graph.py
@@ -172,11 +172,20 @@ class TestCategorical(TestCase):
 
         def _run(dag, u, ys, **kwargs):
 
+
+            def to_cpt(X):
+                return np.exp(
+                    Dirichlet._ensure_moments(
+                        X,
+                        DirichletMoments
+                    ).get_moments()[0]
+                )
+
+
             def _check(X, y):
                 X.update()
                 cpts = {
-                    name:
-                    np.exp(Dirichlet._ensure_moments(config["table"], DirichletMoments).get_moments()[0])
+                    name: to_cpt(config["table"])
                     for (name, config) in dag.items()
                 }
                 for (name, ind) in y.items():
@@ -485,6 +494,34 @@ class TestCategorical(TestCase):
             lambda cpts: {
                 "x": sumproduct("x,axy->x", cpts["x"], cpts["y"], plates_ndim=0),
                 "y": sumproduct("x,axy->axy", cpts["x"], cpts["y"], plates_ndim=1),
+            },
+            [
+                {"x": 1},
+                {"y": [1, 0, 0, 1, 1, 1, 1, 1, 0, 0]},
+                {"x": 1, "y": [1, 0, 0, 1, 1, 1, 1, 1, 0, 0]},
+            ],
+            plates={"a": 10},
+        )
+
+        # Broadcast plates
+        #
+        # x
+        # |
+        # y
+        _run(
+            {
+                "x": {
+                    "table": random(2),
+                },
+                "y": {
+                    "table": random(2, 3),
+                    "given": ["x"],
+                    "plates": ["a"],
+                },
+            },
+            lambda cpts: {
+                "x": sumproduct("x,axy->x", cpts["x"], np.broadcast_to(cpts["y"], (10,2,3)), plates_ndim=0),
+                "y": sumproduct("x,axy->axy", cpts["x"], np.broadcast_to(cpts["y"], (10,2,3)), plates_ndim=1),
             },
             [
                 {"x": 1},